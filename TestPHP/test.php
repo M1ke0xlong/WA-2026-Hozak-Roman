e2e482008be91237cc048280cf9ca0ac6ad40227
Roman Hozak <roman.hozak.021@g.pslib.cz>
2026-02-19 15:46:58 +0100

    #2 web

--- TestPHP/test.php
+++ TestPHP/test.php
@@ -20,7 +20,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>testPHP</title>
 </head>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
